8abd4a787940baa2027ce66c66b31bf6e1e8958d
adorris1 <ashleydorris89@gmail.com>
2016-11-04 10:45:41 -0700

    added config stuff for end point

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   enum authy_status: [:unverified, :onetouch, :sms, :token, :approved, :denied]
-  validates :email,  presence: true, format: { with: /\A.+@.+$\Z/ }, uniqueness: true
+  validates :email,  presence: true, format: { with: /\A.+@.+$\Z/ }
   validates :name, presence: true
   validates :country_code, presence: true
   validates :phone_number, presence: true
