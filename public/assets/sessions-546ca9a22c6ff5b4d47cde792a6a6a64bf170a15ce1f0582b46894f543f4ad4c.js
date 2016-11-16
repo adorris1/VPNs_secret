88c8b2dcd288976cc95770a62c6056b08dfc44c1
Gregg PollackAshley <ashleydorris89@gmail.com>
2016-11-15 17:14:55 -0800

    fick moms comp

---
+++ public/assets/sessions-546ca9a22c6ff5b4d47cde792a6a6a64bf170a15ce1f0582b46894f543f4ad4c.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  $('#login-form').submit(function(e) {
+    e.preventDefault();
+    formData = $(e.currentTarget).serialize();
+    attemptOneTouchVerification(formData);
+  })
+
+  var attemptOneTouchVerification = function(form) {
+    $.post( "/sessions", form, function(data) {
+      $('#authy-modal').modal({backdrop:'static'},'show')
+      if (data.success) {
+        $('.auth-ot').fadeIn()
+        checkForOneTouch();
+      } else {
+        $('.auth-token').fadeIn()
+      }
+    })
+  };
+
+    var checkForOneTouch = function() {
+        var source = new EventSource("/authy/live_status")
+        source.addEventListener("authy_status", function(event) {
+            var data = JSON.parse(event.data);
+            if (data.status === "approved") {
+                source.close();
+                window.location.href = "/account";
+            } else if (data.status === "denied") {
+                showTokenForm();
+                triggerSMSToken();
+            }
+        })
+    };
+
+  var showTokenForm = function() {
+    $('.auth-ot').fadeOut(function() {
+      $('.auth-token').fadeIn('slow')
+    })
+  };
+
+  var triggerSMSToken = function() {
+    $.get("/authy/send_token")
+  };
+})
+
+;
